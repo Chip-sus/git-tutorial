c1abbf3cc07360eb5492bfced9a48735ea300159
Huy Hoang <hgiahuy281000@gmail.com>
2024-06-04 15:30:32 +0300

    Version 3

--- hello.c
+++ hello.c
@@ -3,7 +3,7 @@
 int main()
 {
     printf("\nThis is version 1\n");
-    printf("Now this is version 2\n");
+    printf("Now this is version 3\n");
     printf("Hello World! hehe\n");
     
     printf("\n");                     // just for clarity on terminal
--- types.c
+++ types.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    // Integers hehe
+    // Integers hehe more hehehe
     int positive = 10;
     int negative = -10;
     unsigned int positiveOnly = 20;
